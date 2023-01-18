c57d3ed2d97cc8f064665c8233d5ca2a86c8fa69
oladips <oladips200@gmail.com>
2023-01-18 11:32:42 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 /** @format */
 import "./App.css";
+import About from "./pages/About";
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
